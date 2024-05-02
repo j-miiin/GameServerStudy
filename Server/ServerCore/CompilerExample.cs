8d3df118e1d15d50f73f681c2e2f258bf9969bcf
j-miiin <62470991+j-miiin@users.noreply.github.com>
2024-05-02 15:06:34 +0900

    [섹션1] 컴파일러 최적화 ~ 메모리 배리어 수강

---
+++ Server/ServerCore/CompilerExample.cs
@@ -0,0 +1,34 @@
+﻿namespace ServerCore
+{
+    internal class CompilerExample
+    {
+        volatile static bool _stop = false; // volatile 키워드를 통해 최적화 하지 않도록 강제
+
+        static void ThreadMain()
+        {
+            Console.WriteLine("쓰레드 시작!");
+
+            while (!_stop)
+            {
+
+            }
+
+            Console.WriteLine("쓰레드 종료!");
+        }
+
+        static void Main(string[] args)
+        {
+            Task t = new Task(ThreadMain);
+            t.Start();
+
+            Thread.Sleep(1000);
+
+            _stop = true;
+
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
+            t.Wait();   // Thread.Join()과 같음
+            Console.WriteLine("종료 성공");
+        }
+    }
+}
--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,39 +1,36 @@
 ﻿namespace ServerCore
 {
+    // 메모리 배리어
+    // 1. 코드 재배치 억제
+    // 2. 가시성
+
     internal class Program
     {
-        static void MainThread(object state)
+        int _answer;
+        bool _complete;
+
+        void A()
         {
-            for (int i = 0; i < 5; i++)
-                Console.WriteLine("Hello Thread!");
+            // Store가 2번이므로 Barrier를 2개 사용
+            // Barrier 1은 _answer에 대한 가시성만 보장
+            _answer = 123;
+            Thread.MemoryBarrier(); // Barrier 1
+            _complete = true;
+            Thread.MemoryBarrier(); // Barrier 2
+        }
+
+        void B()
+        {
+            Thread.MemoryBarrier(); // Barrier 3
+            if (_complete)
+            {
+                Thread.MemoryBarrier(); // Barrier 4
+                Console.WriteLine(_answer);
+            }
         }
 
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
-
-            for (int i = 0; i < 5; i++)
-            {
-                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
-                t.Start();
-            }
-
-            //for (int i = 0; i < 4; i++)
-            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
-
-            ThreadPool.QueueUserWorkItem(MainThread);
-
-            //Thread t = new Thread(MainThread);
-            //t.Name = "Test Thread";
-            //t.IsBackground = true;  // Thread를 백그라운드에서 실행하게 함
-            //t.Start();
-            //Console.WriteLine("Waiting for Thread");
-            //t.Join();   // Thread가 끝날 때까지 기다림
-
-            //Console.WriteLine("Hello, World!");
-
-            while (true) { }
         }
     }
 }
