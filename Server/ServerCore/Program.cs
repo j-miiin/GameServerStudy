18b8f82bcbfca169274ccff0d2ba543e4e03cd5d
j-miiin <62470991+j-miiin@users.noreply.github.com>
2024-05-02 14:20:32 +0900

    [섹션0/1] ~쓰레드 생성 수강

---
+++ Server/ServerCore/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ServerCore
+{
+    internal class Program
+    {
+        static void MainThread(object state)
+        {
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("Hello Thread!");
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
+                t.Start();
+            }
+
+            //for (int i = 0; i < 4; i++)
+            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            //Thread t = new Thread(MainThread);
+            //t.Name = "Test Thread";
+            //t.IsBackground = true;  // Thread를 백그라운드에서 실행하게 함
+            //t.Start();
+            //Console.WriteLine("Waiting for Thread");
+            //t.Join();   // Thread가 끝날 때까지 기다림
+
+            //Console.WriteLine("Hello, World!");
+
+            while (true) { }
+        }
+    }
+}
